b5a0702ea603ca355cd4e53763ae90d5f5421c52
nuhundred@gmail.com <nuhundred@gmail.com>
2018-11-27 00:26:34 +0700

    Updates

---
+++ precache-manifest.6a9c1421c997135a53e300a6fa3565a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-setstate-example.io/myapp/public/index.html/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e43a03e376dee2f963e",
+    "url": "/react-setstate-example.io/myapp/public/index.html/static/js/runtime~main.5e43a03e.js"
+  },
+  {
+    "revision": "74699e01201cf3ff649c",
+    "url": "/react-setstate-example.io/myapp/public/index.html/static/js/main.74699e01.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-setstate-example.io/myapp/public/index.html/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "74699e01201cf3ff649c",
+    "url": "/react-setstate-example.io/myapp/public/index.html/static/css/main.b8aee27b.chunk.css"
+  },
+  {
+    "revision": "8fd471d8a1a1f406e590424514c82059",
+    "url": "/react-setstate-example.io/myapp/public/index.html/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-setstate-example.io/myapp/precache-manifest.e0344e7bbbd0a97d564eaea4444e0a67.js"
+  "/react-setstate-example.io/myapp/public/index.html/precache-manifest.6a9c1421c997135a53e300a6fa3565a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-setstate-example.io/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/react-setstate-example.io/myapp/public/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
